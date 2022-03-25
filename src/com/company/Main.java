ff03dda9a7db0c913953fe232cb0b4a33f35251c
unknown <hung.pnpt93@gmail.com>
2022-03-25 10:27:13 +0700

    bt1 class circle

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Circle circle = new Circle();
+        circle.setRadius(3);
+        System.out.println("dien tich S = "+circle.area());
+        System.out.println("chuvi C = "+circle.perimeter());
+    }
+}
